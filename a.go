282f62154ae047e392f466b861c5091da808ef21
aa <email@example.com>
2018-10-16 13:36:56 +0800

    111

--- a.go
+++ a.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"../test/str1"
+	"fmt"
 	"strconv"
 )
 
-func main(){
+func main() {
 	//println("hello,world111")
 	/*
-	var x int32
-	x=100
-	f:=test(x)
-	f()
+		var x int32
+		x=100
+		f:=test(x)
+		f()
 	*/
 	var user str1.User
-	user.Id=1
-	user.Name="张三"
-	user.Age=20
-	user.Add=""
+	user.Id = 1
+	user.Name = "张三"
+	user.Age = 20
+	user.Add = ""
 	fmt.Println(str1.ReturnStr())
-	fmt.Printf("编号:%s,姓名:%s,年龄:%s,地址:%s\n",strconv.Itoa(user.Id),
-		user.Name,strconv.Itoa(user.Age),user.Add)
+	fmt.Printf("编号:%s,姓名:%s,年龄:%s,地址:%s\n", strconv.Itoa(user.Id),
+		user.Name, strconv.Itoa(user.Age), user.Add)
 	Copy1()
 	str1.Ptr()
-	var m=make(map[int32]string)
-	m=str1.User1(1,"张三")
+	var m = make(map[int32]string)
+	m = str1.User1(1, "张三")
+	m[3] = "王五"
 	fmt.Println(m)
 
-
 }
-func Copy1(){
-	s1:=[]int{1,2,3,4,5}
-	s2:=[]int{6,7,8,9}
-	copy(s1,s2)
+func Copy1() {
+	s1 := []int{1, 2, 3, 4, 5}
+	s2 := []int{6, 7, 8, 9}
+	copy(s1, s2)
 	fmt.Println(s1)
 }
-
--- str1/str2.go
+++ str1/str2.go
@@ -1,9 +1,9 @@
 package str1
 
-var m=make(map[int32]string)
-func user(id int32,name string) map[int32]string {
-	m[1]="张三"
-	m[2]="李四"
+var m = make(map[int32]string)
+
+func User1(id int32, name string) map[int32]string {
+	m[1] = "张三"
+	m[2] = "李四"
 	return m
 }
-
